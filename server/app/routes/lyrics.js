63376dde5f3fcc2947b131bc418a6344b7ce624a
Chloe Hwang <c.chloeh@gmail.com>
2017-02-10 10:28:04 -0500

    s

--- server/app/routes/lyrics.js
+++ server/app/routes/lyrics.js
@@ -3,6 +3,17 @@ const router = new express.Router();
 const request = require('request-promise');
 const parser = require('xml2json');
 module.exports = router;
+const express = require('express');
+const getLyrics = require('lyric-get').get;
+const router = new express.Router();
+module.exports = router;
+
+router.get('/:artist/:song', (req, res, next) => {
+    getLyrics(req.params.artist, req.params.song, (err, r) => {
+        if (err) return next(err);
+        res.send({ lyric: r });
+    });
+});
 
 const lyricsAPIPrefix = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect';
 
